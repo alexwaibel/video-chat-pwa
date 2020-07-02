f68c581f3e6d04cafc1f5c7a82eabeae824fa658
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-07-02 14:55:40 -0400

    Updates

---
+++ precache-manifest.2d57e9c17b33da33e4aa63803f4cf451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a33c35029073962dc99ff8e8f877a7f",
+    "url": "/video-chat-pwa/index.html"
+  },
+  {
+    "revision": "8e814c35fb0e599df15d",
+    "url": "/video-chat-pwa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d04a2040cad99864565c",
+    "url": "/video-chat-pwa/static/js/2.7ff4952f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-chat-pwa/static/js/2.7ff4952f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e814c35fb0e599df15d",
+    "url": "/video-chat-pwa/static/js/main.ca1a9143.chunk.js"
+  },
+  {
+    "revision": "31a14c02421c80819ea4",
+    "url": "/video-chat-pwa/static/js/runtime-main.d5f2382f.js"
+  }
+]);
