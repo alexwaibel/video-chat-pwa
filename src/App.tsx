5acb1cd29e20d9eecedba219489826f90e473cf1
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-07-02 14:51:42 -0400

    Initial app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Jitsi from './Jitsi';
+
+function App() {
+  return (
+    <div className="App">
+      <Jitsi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Jitsi.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+function Jitsi() {
+  const [loading, setLoading] = useState(true);
+  const containerStyle = {
+    width: "800px",
+    height: "400px",
+  };
+
+  const jitsiContainerStyle = {
+    display: loading ? "none" : "block",
+    width: "100%",
+    height: "100%",
+  };
+
+  function startConference() {
+    try {
+      const domain = "meet.jit.si";
+      const options = {
+        roomName: "this-is-a-unique-room-name",
+        parentNode: document.getElementById('jitsi-container'),
+        interfaceConfigOverwrite: {
+          filmStripOnly: false,
+        },
+        configOverwrite: {
+          disableSimulcast: false,
+        },
+      };
+
+      const api = new JitsiMeetExternalAPI(domain, options);
+      api.addEventListener("videoConferenceJoined", () => {
+        console.log("Local User Joined");
+        setLoading(false);
+        api.executeCommand("displayName", "MyName");
+      });
+    } catch (error) {
+      console.error("Failed to load Jitsi API", error);
+    }
+  }
+
+  useEffect(() => {
+    // verify the JitsiMeetExternalAPI constructor is added to the global..
+    if (window.JitsiMeetExternalAPI) startConference();
+    else alert("Jitsi Meet API script not loaded");
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      {loading && <span>Loading...</span>}
+      <div id="jitsi-container" style={jitsiContainerStyle} />
+    </div>
+  );
+}
+
+export default Jitsi;
